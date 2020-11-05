0b98b32c340033e7ccd510d384badb2db6fa3f3f
v-for-vasiliy <v-for-vasiliy@users.noreply.github.com>
2020-11-05 23:14:10 +0300

    Добавил скрипт закрытия модалки при нажатии клавиши esc

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ feedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackModal.classList.remove("modal-show");
 });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (feedbackModal.classList.contains("modal-show")) {
+        evt.preventDefault();
+        feedbackModal.classList.remove("modal-show");
+      }
+    }
+  });
